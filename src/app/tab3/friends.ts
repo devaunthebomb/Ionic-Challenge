529299f49380f1f6ada6dd8b64e9f5bedb082b4c
TXT-42 <txt-42@TXT-42.local>
2021-07-08 09:52:21 -0700

    first commit

---
+++ src/app/tab3/friends.ts
@@ -0,0 +1,23 @@
+let arr: any[] = [
+    {
+        name: "Joseph Joestar",
+        imgSrc: "https://i.pinimg.com/474x/8a/07/32/8a0732eb755d00c004d495252430428b.jpg",
+        phoneNumber: "324234342423423"
+    },
+    {
+        name: "Saitama",
+        imgSrc: "https://media.comicbook.com/2017/11/saitama-1061551-1280x0.jpg",
+        phoneNumber: "23413242342342"
+    },
+    {
+        name: "Invincible",
+        imgSrc: "https://observer.com/wp-content/uploads/sites/2/2021/05/INVI_S1_FG_108_Still375.jpg?quality=80&w=635",
+        phoneNumber: "342342342342"
+    },
+    {
+        name: "Patrick Star",
+        imgSrc: "https://www.tvguide.com/a/img/resize/fcd57869b96ee0dab8f67c578b1655326b569991/hub/2019/07/16/97d74a7e-c71a-4b0b-a352-532d935edfe3/190715-spongebob-12-pink-loser-2070x1380.jpg?fit=crop&height=1080&width=1920",
+        phoneNumber: "291837138273853"
+    }
+]
+ export default arr;
